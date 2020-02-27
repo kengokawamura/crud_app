7d657c9af0015f8572f0e201cc8327e5204a0456
K.Kawamura <kengokawamura0@gmail.com>
2020-02-27 18:56:11 +0900

    read method added

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,5 +6,6 @@ const product_controller = require('../controllers/product.controller');
 
 router.get('/test', product_controller.test);
 router.post('/create', product_controller.product_create);
+router.get('/:id', product_controller.product_details);
 
 module.exports = router;
