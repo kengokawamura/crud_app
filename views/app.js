340d760a52d33ced1cab62da752f686e4fd7066d
K.Kawamura <kengokawamura0@gmail.com>
2020-02-27 18:25:23 +0900

    add post method

--- views/app.js
+++ views/app.js
@@ -1,10 +1,27 @@
-//app.js
+// app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const product = require('../routes/product.route'); // Imports routes for the products
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = `mongodb://admin:sa@cluster0-shard-00-00-etdd9.mongodb.net:27017,cluster0-shard-00-01-etdd9.mongodb.net:27017,cluster0-shard-00-02-etdd9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+`;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB,{ useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
+
 let port = 1234;
+
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
